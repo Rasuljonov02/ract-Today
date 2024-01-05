9b2327eb746f0d57436ceb27cad070dfc686dbb1
Muhammad <Muhammad@mx>
2024-01-05 21:54:26 +0500

    ddd

--- src/index.tsx
+++ src/index.tsx
@@ -1,9 +1,19 @@
-
-import ReactDOM from 'react-dom';
-import Main from './main';
-
-import 'assets/style.css';
-
-const root = ReactDOM.createRoot(document.getElementById('root')!);
-
-root.render(<Main />);
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { Provider } from "react-redux";
+import { BrowserRouter } from "react-router-dom";
+import store from "./store/index";
+import App from "./App";
+import "./index.css";
+const root = ReactDOM.createRoot(
+  document.getElementById("root") as HTMLElement
+);
+root.render(
+        <React.StrictMode>
+            <Provider store={store}>
+                <BrowserRouter>
+                            <App/>
+                </BrowserRouter>
+            </Provider>
+        </React.StrictMode>
+);
