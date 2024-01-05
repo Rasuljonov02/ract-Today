dc4292b15d7d0b0c14330afbc211fe0e35d93418
Muhammad <Muhammad@mx>
2024-01-05 20:16:15 +0500

    dsd

--- src/components/index.tsx
+++ src/components/index.tsx
@@ -1,13 +1,14 @@
+import React, { Component } from 'react';
 import Left from './left';
 import Right from './right';
 
-function Project() {
-  return (
-    <div className="w-[100%] flex items-center">
-      <Left />
-      <Right />
-    </div>
-  );
+export default class YourComponentName extends Component {
+  render() {
+    return (
+      <div className="w-[100%] flex items-center">
+        <Left />
+        <Right />
+      </div>
+    );
+  }
 }
-
-export default Project;
